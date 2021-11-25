9a3196fe2afa013e6c755a66a265d8be159f51a1
Tumworobere <annaht.annah@gmail.com>
2021-11-25 10:54:37 +0300

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
+  mode: 'development',
   devServer: {
     static: './dist',
   },
@@ -15,7 +15,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
