67d08ffa1281125b7ca13b05ec76a2a8028e2673
Tumworobere <annaht.annah@gmail.com>
2021-11-27 00:46:29 +0300

    Modify Js files

--- src/api.js
+++ src/api.js
@@ -1,29 +1,33 @@
-const saveToApi = async (username, userscore) => {
-  try {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/v4n9rcNTL0SXbCD8IPLG/scores/', {
-      method: 'POST',
-      body: JSON.stringify({
-        user: username,
-        score: userscore,
-      }),
-      headers: {
-        'content-type': 'application/json; charset=UTF-8',
+const gameScoreUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Qc8Crttfp6u4HrkOhxwH/scores';
+const userName = document.getElementById('name');
+const score = document.getElementById('score');
+
+// GET to get a list of scores for the given game
+const getScore = async () => {
+  const scores = await fetch(gameScoreUrl, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return scores.json();
+};
+// POST to create a new score for the given game
+const postScore = async () => {
+  const postScores = await fetch(gameScoreUrl, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(
+      {
+        user: userName.value,
+        score: score.value,
       },
-    });
-    return await response.json();
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    ),
+
+  });
+  return postScores.json();
 };
 
-const getScores = async () => {
-  try {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/v4n9rcNTL0SXbCD8IPLG/scores/');
-    const data = await response.json();
-    return data.result;
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-export { getScores, saveToApi };
+export { getScore, postScore };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+import updateList from './scorelist.js';
+import { getScore, postScore } from './api.js';
+
+const scoreBoard = document.getElementById('scoreboard');
+const refreshBtn = document.getElementById('refresh-btn');
+const submitForm = document.querySelector('.input-score');
+const player = document.getElementById('name');
+const score = document.getElementById('score');
+
+async function updateScore() {
+  const fetchAPI = await getScore();
+  const updatedList = fetchAPI.result;
+  updatedList.sort((a, b) => a.score - b.score);
+  updateList(updatedList);
+}
+
+refreshBtn.addEventListener('click', async () => {
+  scoreBoard.innerHTML = '';
+  await updateScore();
+});
+
+submitForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const playerName = player.value;
+  const playerScore = score.value;
+  await postScore({ playerName, playerScore });
+  player.value = '';
+  score.value = '';
+});
